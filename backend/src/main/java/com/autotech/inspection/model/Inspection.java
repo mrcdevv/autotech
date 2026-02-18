ce1fa7aaa85e5174f1598c1acfccb2f1b49054cd
mrcdevv <marcoemorales211@gmail.com>
2026-02-18 12:30:00 -0300

    fix: resolve MultipleBagFetchException by changing Inspection.items to Set

--- backend/src/main/java/com/autotech/inspection/model/Inspection.java
+++ backend/src/main/java/com/autotech/inspection/model/Inspection.java
@@ -16,8 +16,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "inspections")
@@ -38,7 +38,7 @@ public class Inspection extends BaseEntity {
 
     @OneToMany(mappedBy = "inspection", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
-    private List<InspectionItem> items = new ArrayList<>();
+    private Set<InspectionItem> items = new HashSet<>();
 
     @Override
     public boolean equals(Object o) {
